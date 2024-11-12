5c32062e14da88b751935700f6d914c44f63d480
Slav Mosoyan <vladanik1999@gmail.com>
2024-11-12 21:21:54 +0300

    Initial commit

---
+++ src/demoPizzas.js
@@ -0,0 +1,9 @@
+const demoPizzas = [
+  { title: "peperoni", price: 320, img: "pizza-1.jpg", id: 1 },
+  { title: "Margarita", price: 550, img: "pizza-2.jpg", id: 2 },
+  { title: "Sausage", price: 420, img: "pizza-3.jpg", id: 3 },
+  { title: "3 Cheese", price: 120, img: "pizza-4.jpg", id: 4 },
+  { title: "Mixed", price: 3320, img: "pizza-5.jpg", id: 5 },
+];
+
+export default demoPizzas;
